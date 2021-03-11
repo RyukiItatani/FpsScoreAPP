6df6a1dc2d147167dbcdbdee98f0c1c414a2bd54
Itatani Ryuki <rkukbt20000129@gmail.com>
2021-03-11 19:50:02 +0900

    update commit

--- myapp/settings.py
+++ myapp/settings.py
@@ -108,13 +108,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'Asia/Tokyo'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = False
+USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
